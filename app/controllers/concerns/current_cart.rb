f4eabf597ad11bd64990116ca5e7a924d6b46198
Austin Dulay <dulayaustin@gmail.com>
2016-07-12 03:44:50 +0800

    Create smart cart

---
+++ app/controllers/concerns/current_cart.rb
@@ -0,0 +1,19 @@
+module CurrentCart
+  extend ActiveSupport::Concern
+
+  private
+
+    def set_cart
+      @cart = Cart.find(session[:cart_id])
+    rescue ActiveRecord::RecordNotFound
+      @cart = Cart.create
+      session[:cart_id] = @cart.id
+    end
+
+    def increment_count
+      if session[:counter].nil?
+        session[:counter] = 0
+      end
+      session[:counter] += 1
+    end
+end
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,8 @@
 class StoreController < ApplicationController
+  include CurrentCart
+
   def index
     @products = Product.order(:title)
+    @count = increment_count
   end
 end
